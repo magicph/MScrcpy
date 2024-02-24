e043f12da28ca13cbaa0f7bc3f0ccba9f368fbc4
denverku <magicph26@gmail.com>
2024-02-24 16:59:41 +0800

    Update sndcpy.bat

--- QtScrcpy/sndcpy/sndcpy.bat
+++ QtScrcpy/sndcpy/sndcpy.bat
@@ -16,28 +16,21 @@ echo Waiting for device %1...
 %ADB% %serial% wait-for-device || goto :error
 echo Find device %1
 
-for /f "delims=" %%i in ('%ADB% %serial% shell pm path com.rom1v.sndcpy') do set sndcpy_installed=%%i
-if "%sndcpy_installed%"=="" (
-    echo Install %SNDCPY_APK%... 
-    %ADB% %serial% uninstall com.rom1v.sndcpy || echo uninstall failed
-    %ADB% %serial% install -t -r -g %SNDCPY_APK% || goto :error
-    echo Install %SNDCPY_APK% success
-)
 
 echo Request PROJECT_MEDIA permission...
-%ADB% %serial% shell appops set com.rom1v.sndcpy PROJECT_MEDIA allow
+%ADB% %serial% shell appops set com.tkstudio.mscrcpy PROJECT_MEDIA allow
 
 echo Forward port %SNDCPY_PORT%...
 %ADB% %serial% forward tcp:%SNDCPY_PORT% localabstract:sndcpy || goto :error
 
 echo Start %SNDCPY_APK%...
-%ADB% %serial% shell am start com.rom1v.sndcpy/.MainActivity || goto :error
+%ADB% %serial% shell am start com.tkstudio.mscrcpy/.MainActivity || goto :error
 
 :check_start
 echo Waiting %SNDCPY_APK% start...
 ::timeout /T 1 /NOBREAK > nul
 %ADB% %serial% shell sleep 0.1
-for /f "delims=" %%i in ("%ADB% shell 'ps | grep com.rom1v.sndcpy'") do set sndcpy_started=%%i
+for /f "delims=" %%i in ("%ADB% shell 'ps | grep com.tkstudio.mscrcpy'") do set sndcpy_started=%%i
 if "%sndcpy_started%"=="" (
     goto :check_start
 )
